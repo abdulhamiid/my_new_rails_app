d7b41a65a906a8b5a87326c8d4e37165d273cb6d
abdulhamiid <adioabdulhamid@yahoo.com>
2022-12-09 07:16:25 +0100

    Load data from api with redux

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,14 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './components/redux/store';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+  </React.StrictMode>,
 );
