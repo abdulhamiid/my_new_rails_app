8a38bdb4112314e121340460b6109da7284caeb5
abdulhamiid <adioabdulhamid@yahoo.com>
2022-12-09 21:49:45 +0100

    Fix all errors

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,8 +7,8 @@ import store from './components/redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
